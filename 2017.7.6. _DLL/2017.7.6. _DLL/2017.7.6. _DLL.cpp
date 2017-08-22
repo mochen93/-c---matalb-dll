d9cf44b287b54432c474a6842300bb666ae31b00
mochen93 <30330525+mochen93@users.noreply.github.com>
2017-08-22 11:36:34 +0800

    c++ 基础调用matlab
    c++ 基础调用matlab

---
+++ 2017.7.6. _DLL/2017.7.6. _DLL/2017.7.6. _DLL.cpp
@@ -0,0 +1,13 @@
+// 2017.7.6. _DLL.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "testdll.h"
+#include <iostream>
+using namespace std;
+int Add(int plus1, int plus2)
+{
+	int add_result = plus1 + plus2;
+	return add_result;
+}
+
